e53251642aac4a85665b9d47d1cd6b28a10f2ae1
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-14 15:16:22 +0530

    router working

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,8 +1,27 @@
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Layout from './Layout.jsx'
+import { Home, About, Contact } from './components/index.js'
+
+const router = createBrowserRouter (
+  createRoutesFromElements(      // creating nesting of routes inside Layout
+    <Route path = '/' element = {<Layout/>}>
+      <Route path = '' element = {<Home/>}/>
+      <Route path = 'about' element = {<About/>}/>
+      <Route path = 'contact' element = {<Contact/>}/>
+      {/* <Route path = 'about' element = {<About/>}/>
+      <Route path = 'about' element = {<About/>}/> */}
+
+
+
+    </Route>
+  )
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
