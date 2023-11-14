34ac0f94dc1e35af9aaeaaf45b1cdc612afbb14b
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-14 14:07:05 +0530

    header footer home component created

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Header } from './components'
+import { Header, Footer, Home } from './components'
 
 import './App.css'
 
@@ -9,6 +9,8 @@ function App() {
   return (
   <><div className='bg-red-400 p-4 rounded-lg'>React Router</div>
   <Header></Header>
+  <Home></Home>
+  <Footer></Footer>
   </>
   )
 }
--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -1,3 +1,4 @@
+import { RouterProvider } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -5,6 +6,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </React.StrictMode>,
 )
