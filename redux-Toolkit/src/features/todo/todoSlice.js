e185bb02ad4b34b834c7ebab954dbe6a8053d3df
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-20 09:31:50 +0530

    state and reducers created

---
+++ redux-Toolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,19 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+  todos: [{id: 1, text:'Hello Todo' }]
+};
+
+export const todoSlice = createSlice({
+  name : 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state,action) => {
+     const  todo=  {id: nanoid(), text:action.payload }
+     state.todos.push(todo)
+
+    },
+    removeTodo: () => {},
+  }
+});
+
