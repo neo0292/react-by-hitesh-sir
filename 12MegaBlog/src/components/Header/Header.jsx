979d6bf0abcdb51e9cd95365aa8dcae8659ce4ad
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-25 23:52:57 +0530

    component updated

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Logo, LogoutBtn, Container } from '../index'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 
 function Header() {
   return (
---
+++ 12MegaBlog/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useDispatch } from 'react-redux' //send Data to  store
+import  authService  from '../../appwrite/auth'   // update database
+import { logout } from '../../store/authSlice'  // 
+
+function LogoutBtn() {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+      authService.logout().then(() => {
+        dispatch(logout());
+      })
+  }
+  return (
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
+  )
+}
+
+export default LogoutBtn
