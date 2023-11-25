7e3f44cbc6b78dcb9171b6281aa66a615ecd0564
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-25 19:28:00 +0530

    create update delete post

---
+++ 12MegaBlog/src/appwrite/config.js
@@ -0,0 +1,130 @@
+import conf from '../conf/conf';
+import { Client, Databases, Storage, Query,ID } from 'appwriter'
+
+export class Service {
+   client = new Client();
+   databases;
+   bucket;
+
+   constructor(){
+    this.client
+        .setEndPoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+   }
+
+   async createPost({title, slug, content, featuredImage,status,userId}){
+    try {
+      return this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId, 
+        slug,
+        {title, content, featuredImage, status, userId}
+        );
+      
+    } catch (error) {
+      console.log('Appwrite Serice::CreatePost:: error: ' + error);
+    }
+   
+   }
+
+   async updatePost(slug,{title,  content, featuredImage,status}){
+    try {
+      return await this.databases.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {title, content, featuredImage, status}
+      );
+      
+    } catch (error) {
+      console.log('Appwrite Serice::UpdatePost:: error: ' + error);
+    }
+   }
+
+   async deletePost(slug){
+    try {
+      await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+        );
+      return true;  
+    } catch (error) {
+      console.log('Appwrite Serice::DeletePost:: error: ');
+      return false; 
+    }
+   }
+
+   async getPost(slug){
+    try {
+      return this.databases.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      )
+    } catch (error) {
+      console.log('Appwrite Serice::GetPost:: error: ');
+      return false; 
+      
+    }
+   }
+
+   async getPosts(queries = [Query.equal("status", "active")]){//status:key active:value pairs input to query
+    try {
+      return this.databases.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries,
+      )
+    } catch (error) {
+      console.log('Appwrite Serice::GetAllPost:: error: ');
+      return false; 
+      
+    }
+   }
+
+   // fileUpload Service
+
+   async uploadFile(file){
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log('Appwrite Serice::fileUpload:: error: ');
+      return false;
+    }
+   }
+
+   async deleteFile(fileId){
+    try {
+      await this.bucket.deleteFile(
+        conf.appwriteBucketId,
+        fileId);
+      return true
+    } catch (error) {
+      console.log('Appwrite Serice::deleteFile:: error: ');
+      return false;
+    }
+   }
+
+   getFilePreview(fileId){
+    try {
+      return this.bucket.getFilePreview(
+        conf.appwriteBucketId,
+        fileId);
+      
+    } catch (error) {
+      console.log('Appwrite Serice::getFilePreview:: error: ');
+      
+    }
+   }
+}
+
+
+const service = new Service();
+export default service;
