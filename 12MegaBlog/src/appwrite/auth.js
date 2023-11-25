6e08a14e8629848fde180034c63f90c195993c1d
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-25 13:18:31 +0530

    Appwrite Auth completed

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,64 @@
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthSevice {
+  client = new Client();
+  account;
+
+  constructor(){
+    this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);    
+  }
+
+  async createAccount({email, password, name}){
+    try {
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+      if (userAccount) {
+        this.login({email, password});
+        
+      } else {
+        return userAccount;
+        
+      }
+  
+    } catch (error) {
+      throw error;
+      
+    }
+    
+  }
+
+
+  async login({email,password}){
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+      
+    }
+  }
+
+  async getCurrentUser(){
+    try {
+      await this.account.get();
+    } catch (error) {
+      console.log('Appwrite Service Error:: getCurrentUser :: Error ', error);
+    }
+
+    return null;
+  }
+  async logout(){
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log('Appwrite Service Error:: logout ::error',error);
+      
+    }
+  }
+}
+
+const authService = new AuthSevice();
+
+export default authService;
