800f04de8b500278d68e05e024b12f1f367a1a68
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-25 11:07:22 +0530

    .env file setup done

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import conf from './conf/conf'
+import './App.css'
+
+function App() {
+  console.log(conf.appwriteBucketId);
+  
+ 
+
+  return (
+   <>
+   <h1>App with appwrite</h1>
+   </>
+  )
+}
+
+export default App
