e5b95755cb9bb79a21e81c4087b7fe801844dfc5
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-11-26 18:24:48 +0530

    input, button, select components created

---
+++ 12MegaBlog/src/components/Button.jsx
@@ -0,0 +1,19 @@
+//custom button
+import React from 'react'
+
+function Button({
+  children,
+  type = 'button',
+  bgColor ='bg-blue-600',
+  textColor = 'text-white',
+  className = '', // user can pass some classes
+  ...props}) //some extra attributes/properties passed by user we consider it as props 
+  {
+  return (
+    <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}{...props}>
+      {children}
+    </button>
+  )
+}
+
+export default Button
